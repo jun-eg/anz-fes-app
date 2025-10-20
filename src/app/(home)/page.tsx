074b7ccbd0318d80fbae80dbc807719a859b3185
1F10230014 <s1f102300144@iniad.org>
2025-10-20 21:32:31 +0900

    ホーム画面追加

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>ホーム画面</div>;
+  return (
+    <div className="p-8 flex flex-col items-center gap-4 font-mono h-full">
+      <div className="text-xl pb-10">あんず。シフト提出サイト</div>
+      <div className="flex flex-col gap-4  h-full ">
+        <Button variant="outline">
+          <Link href="https://docs.google.com/forms/d/e/1FAIpQLSf2wO3Kt0e20jxupo97TP7r2Thm-qy-OhXI3MOQhyncdXeJkw/viewform?usp=header">
+            シフト提出
+          </Link>
+        </Button>
+        <Button variant="outline">
+          <Link href="/shiftLogs">シフト希望確認</Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
